7b5208a3006fc8d6517237065891ec08b547340d
Leonardo Castiglioni <leonardocastiglioni@gmail.com>
2016-04-05 16:41:07 -0300

    Update 1 - Exercise 01 Topic3

--- src/main/java/com/globant/bootcamp/tae/Ex01.java
+++ src/main/java/com/globant/bootcamp/tae/Ex01.java
@@ -1,6 +1,9 @@
 package com.globant.bootcamp.tae;
 
 
+import java.util.NoSuchElementException;
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -30,6 +33,43 @@ public class Ex01
 		} catch (Exception e) {
 		}
 	}
+	
+	public WebElement findElement(WebDriver driver, By locator, int seconds) {
+		try {
+			seconds = (seconds == 0 ? 1 : seconds);
+			this.driver.manage().timeouts().implicitlyWait(seconds, TimeUnit.SECONDS);
+			return driver.findElement(locator);
+		} catch (NoSuchElementException e) {
+			return null;
+		}
+	}
+
+	
+	// A version of waitWhileElementPresentXpath
+	// This one reverses the logic waiting for the element to the loaded
+	public boolean waitForElement(String locator, int maxSeconds) {
+		long initialMillis = System.currentTimeMillis ();
+		boolean loaded = false;
+		boolean timeout = false ;
+		do {
+			try {
+				this.driver.findElement(By.xpath(locator));
+				loaded= true;
+			} catch (Exception e){	}
+			
+			long currentMillis = System. currentTimeMillis ();
+			if ((currentMillis - initialMillis)/ 1000 >= maxSeconds){
+				timeout = true ;
+			}
+		} while (!loaded && !timeout);
+		if (timeout){
+			System.out.println("Timeout: "+ maxSeconds + " seconds have passed waiting for page");
+			return false ;
+		} else {
+			return true ;
+		}
+	} 
+	
 	@Test(
 			description = "Exercise 01"
 			)	
@@ -38,20 +78,30 @@ public class Ex01
 		driver.get("http://labrujula24.com/noticias/2016/23099_Mauricio-Macri-hablo-sobre-su-sociedad-offshore-qEs-una-operacion-legalq");
 		
 		// iframe locator
-		String locator = "//iframe[@class=\"fb_ltr\"]";
+		String locator = "//iframe[@class='fb_ltr']";
+
+		// If reaches timeout then asserts false and return
+		if (!this.waitForElement(locator, 20)) {
+			Assert.assertTrue(false);
+			return;
+		}
+			
 		WebElement fb_iframe = driver.findElement(By.xpath(locator));
 		
 		// switch to internal frame
 		this.driver.switchTo().frame(fb_iframe).switchTo();
 		
 		// location of first comment
-		String first_comment = "/html/body/div[1]/div/div/div/div/div[3]/div[1]";
+		String first_comment = "//div[contains(@class, '_3-8y')][1]";
 		String comment_text = this.driver.findElement(By.xpath(first_comment)).getText();
+		
+		// return driver to its default context
+		driver.switchTo().defaultContent();
 		
 		// check the comment text on standard output
 		System.out.println("Noticia: " + comment_text);
 		
-		// assert
+		// Assert
 		Assert.assertTrue(comment_text.contains("Noticia"));
 	}
 
